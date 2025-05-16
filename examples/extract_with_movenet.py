3fcf8f4f504497df9799700d6818971ef5da43a3
Ana Carolina da Costa Alves <up202106111@up.pt>
2025-05-16 12:56:17 +0100

    Update extract_with_movenet.py
    ALL WELL

--- examples/extract_with_movenet.py
+++ examples/extract_with_movenet.py
@@ -1,12 +1,21 @@
 import os
 import sys
+<<<<<<< Updated upstream
 # Add the project root to sys.path
+=======
+
+# Add the src directory to the Python path
+>>>>>>> Stashed changes
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.pose_extraction.movenet_extractor import MoveNetPoseExtractor
 from src.data_processing.landmark_processor import landmarks_to_dataframe, save_landmarks
 
 # Set input
+<<<<<<< Updated upstream
 input_path = "data/3DPW/raw/imageFiles/imageFiles/outdoors_slalom_00"    # <-- Update this path
+=======
+input_path = r"D:\JPC_Datasets(obsolete)\BioImAnalysis_project_dataset\BioImAnalysis_project_dataset\BioImAnalysis_project_dataset_VIDEOS\automotor_MP4\IM706-2_sz03_kinect.mp4"  # <-- Update this path
+>>>>>>> Stashed changes
 
 # Extract
 extractor = MoveNetPoseExtractor()
@@ -14,7 +23,7 @@ landmarks = extractor.extract_landmarks(input_path)
 df = landmarks_to_dataframe(landmarks, model_name="movenet")
 
 # Prepare output path
-output_folder = "data/3DPW/processed/" # <-- Update this path
+output_folder = "data/keypoints/" # <-- Update this path
 os.makedirs(output_folder, exist_ok=True)
 
 # Create a safe identifiable output filename
